4a31e0400bc863a39646defe198e8a9a21824c3b
Carlos de Jesús Kubota Pérez <ckubota@quaxar.com>
2021-08-06 23:34:29 -0500

    add commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,35 @@
 class ArticlesController < ApplicationController
-  def new
-      @article = Article.new
-  
-  end
-  def create 
-     @article = Article.create(title: params[:article][:title])
+  before_action :find_article, except: [:new,:create]
+    def index
+    @articles = Article.all
+    end
+    def show
+    @article = Article.find(params[:id])
+    end
+    def edit
+    @article = Article.find(params[:id])
+    
+    end
+    def delete
+
+    @article.destroy
+    redirect_to root_path
+
+    end
+    def update
+    
+    @article.update(title: params[:article][:title],content: params[:article][:content])
      render json: @article
-  end
+    end
+    def new
+    end
+    def create 
+     @article = Article.create(title: params[:article][:title],content: params[:article][:content])
+     render json: @article
+    end
+
+    def find_article
+      @article = Article.find(params[:id])
+    end
   end
   
